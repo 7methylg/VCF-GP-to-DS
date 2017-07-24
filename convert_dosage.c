f5117d70674623209ba56fa1c8c97191657419fc
7methylg <28456992+7methylg@users.noreply.github.com>
2017-07-24 15:39:00 -0400

    Add files via upload

---
+++ convert_dosage.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+
+FILE *fp;
+
+char command[500]; 
+snprintf(command, 500, "bcftools query -f '%%INFO/AF\n[%%GP\n]' %s", argv[1]);
+
+fp = popen(command , "r");
+
+if (fp == NULL) {
+	printf("Unable to execute bcftools command");
+}
+
+FILE *info;
+char file[70];
+snprintf(file, 70, "%s.infos", argv[1]);
+info = fopen(file, "r");
+
+FILE *vcf;
+char final[70];
+snprintf(final, 70, "dosage-%s", argv[1]);
+vcf = fopen(final, "a");
+
+int line_read = -1;
+float dosage_line[44704];
+char line[50];
+char info_line[847400];
+char *info_l = info_line;
+char* extra;
+float GP[3];
+float af;
+
+while(fgets(line, 50 , fp) != NULL){
+
+	if (line_read == -1){
+		af = (float) atof(line);
+		//printf("maf %f\n", af);
+	}
+	else{
+		
+		GP[0] = strtof(strtok(line, ","), &extra);
+		//printf("first: %f\n", GP[0]);
+		GP[1] = strtof(strtok(NULL, ","), &extra);
+		//printf("second: %f\n", GP[1]);
+		GP[2] = strtof(strtok(NULL, ","), &extra);
+		//printf("third: %f\n", GP[2]);
+
+		if (af >= .5){
+			dosage_line[line_read] = GP[0] + GP[0] + GP[1];
+		}			
+		else{
+			dosage_line[line_read] = GP[2] + GP[2] + GP[1];
+		}
+
+	}
+
+	line_read ++;
+
+	if (line_read == 44704){
+
+		fgets(info_line, 847400, info);
+		fprintf(vcf, "%s\t.\tDS", strtok(info_line, "\n"));
+
+		for (int i=0; i<=44703; i++){
+			fprintf(vcf, "\t%f", dosage_line[i]);
+		}
+
+		fputs("\n", vcf);
+		line_read = -1;
+	}
+ 
+}
+
+fclose(info);
+fclose(vcf);
+pclose(fp);
+
+return 0;
+
+}
---
+++ dosage_convert.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+file=$1
+
+$lines=`bcftools view -h "$file" | wc -l`
+bcftools view -h "$file" | head -$((lines-1)) >> dosage-"$file"
+echo "##FORMAT<ID=DS,Number=1,Type=Float,Description=\"Minor allele dosage\">" >> dosage-"$file"
+bcftools view -h "$file" | tail -1 >> dosage-"$file"
+
+bcftools query -f '%CHROM\t%POS\t%ID\t%REF\t%ALT\t%QUAL\t%FILTER\n' "$file" > "$file".infos
+
+echo "beginning conversion $file"
+
+./convert "$file"
+
+echo "conversion done. zipping dosage-$file..."
+
+bgzip dosage-"$file"
+tabix dosage-"$file"
+rm "$file".infos
+
+echo "$file zipped and ready to go!"
+
+echo "beginning annotation $file"
+
+bcftools annotate -a dosage-"$file" -c FORMAT/DS "$file" | bgzip -c > dosage."$file"
+
